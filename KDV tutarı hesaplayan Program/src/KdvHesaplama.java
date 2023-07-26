dadd75f7f440acbc9584ed54f82ccf6ba4f24141
Serkan Özçelik <serkanozcelik98@gmail.com>
2023-07-26 10:23:25 +0300

    Patika proje

--- KDV tutarı hesaplayan Program/src/KdvHesaplama.java
+++ KDV tutarı hesaplayan Program/src/KdvHesaplama.java
@@ -3,36 +3,34 @@ import java.util.Scanner;
 public class KdvHesaplama {
 
 	public static void main(String[] args) {
-		
-	
-		Scanner input = new Scanner(System.in);
-		int ürünFiyat,kdv,ürünKdv=0;
-		
-	
-		System.out.println("Ürün fiyatını Giriniz: ");
-		ürünFiyat=input.nextInt();
-		
-		if(0<ürünFiyat && ürünFiyat<=1000) 
-		{
-		
-			kdv=18/100;
-			
-			ürünKdv=ürünFiyat*kdv;
-			
-			System.out.println("KDV'li Fiyat: "+ürünKdv);
-			
-			
-		}else if (ürünFiyat>1000) 
-		{
-			kdv=8/100;
-			
-ürünKdv=ürünFiyat*kdv;
-			
-			System.out.println("KDV'li Fiyat: "+ürünKdv);
 
-			
+		Scanner input = new Scanner(System.in);
+
+		double tutar, kdv, kdvTutar, ürünKdv;
+
+		System.out.print("Ürün fiyatını giriniz: ");
+		tutar = input.nextDouble();
+
+		if (tutar > 0 && tutar < 1000) {
+			kdv = 0.18;
+
+			kdvTutar = tutar * kdv;
+
+			ürünKdv = kdvTutar + tutar;
+
+			System.out.println("KDV'li Ürün Fiyatı: " + ürünKdv);
+
+		} else if (tutar > 1000) {
+
+			kdv = 0.8;
+
+			kdvTutar = tutar * kdv;
+
+			ürünKdv = kdvTutar + tutar;
+
+			System.out.println("KDV'li Ürün Fiyatı: " + ürünKdv);
+
 		}
-		
 
 	}
 
